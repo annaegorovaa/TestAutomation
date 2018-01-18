6a8fea63969f39b1b6c5ab57baa45664ca3a3d11
annaegorovaa <anju.egorova@gmail.com>
2018-01-18 11:47:22 +0300

    hw2 ex1 with corrections

--- src/test/java/homework2/DP4TextsTest.java
+++ src/test/java/homework2/DP4TextsTest.java
@@ -3,40 +3,28 @@ package homework2;
 import base.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.List;
+
+import static org.testng.Assert.assertEquals;
 
 public class DP4TextsTest extends TestBase {
 
     @DataProvider(parallel = true)
     public Object[][] dp() {
         return new Object[][] {
-                {"To include good practices\n" +
-                        "and ideas from successful\n" +
-                        "EPAM projec"},
-                {"To be flexible and\n" +
-                        "customizable"},
-                {"To be multiplatform"},
-                {"Already have good base\n" +
-                        "(about 20 internal and\n" +
-                        "some external projects),\n" +
-                        "wish to get more…"}
+                {0, "To include good practices and ideas from successful EPAM projec"},
+                {1, "To be flexible and customizable"},
+                {2, "To be multiplatform"},
+                {3, "Already have good base (about 20 internal and some external projects), wish to get more…"}
         };
     }
 
     @Test(dataProvider = "dp")
-    public void assert4TextsUsingDPTest(String s) {
-        List<WebElement> elements = driver.findElements(By.className("benefit-txt"));
-        boolean textFound = false;
-        for (WebElement element: elements) {
-            if (element.getText().equals(s)) {
-                textFound = true;
-                break;
-            }
-        }
-        Assert.assertTrue(textFound);
+    public void assert4TextsUsingDPTest(int index, String text) {
+        List<WebElement> elements = driver.findElements(By.xpath("//*[@class='benefit-txt']"));
+        assertEquals(elements.get(index).getText().replaceAll("\n", " "), text);
     }
 }
