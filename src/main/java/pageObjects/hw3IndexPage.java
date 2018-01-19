9b95d7f00d3466d0bfef3e3d68bbcc4f6e805812
annaegorovaa <anju.egorova@gmail.com>
2018-01-19 10:21:55 +0300

    hw3

---
+++ src/main/java/pageObjects/hw3IndexPage.java
@@ -0,0 +1,79 @@
+package pageObjects;
+
+import enums.IndexPageTextsEnum;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class hw3IndexPage {
+
+    @FindBy(css = ".uui-profile-menu .dropdown-toggle")
+    private WebElement loginFormButton;
+
+    @FindBy(css = "#Login")
+    private WebElement loginInput;
+
+    @FindBy(css = "#Password")
+    private WebElement passwordInput;
+
+    @FindBy(css = ".form-horizontal [type='submit']")
+    private WebElement submitButton;
+
+    @FindBy(css = ".uui-profile-menu span")
+    private WebElement userName;
+
+    @FindBy(css = ".benefit-icon")
+    private List<WebElement> benefitIcon;
+
+    @FindBy(css = ".benefit-txt")
+    private List<WebElement> benefitText;
+
+    @FindBy(css = "div .main-title")
+    private WebElement header;
+
+    @FindBy(css = "div .main-txt")
+    private WebElement mainText;
+
+    public void login(String name, String password) {
+        loginFormButton.click();
+
+        loginInput.sendKeys(name);
+        passwordInput.sendKeys(password);
+        submitButton.click();
+    }
+
+    public void checkPageTitle(WebDriver driver) {
+        assertEquals(driver.getTitle(), "Index Page");
+    }
+
+    public void checkUserIsLoggedIn(WebDriver driver) {
+        assertEquals(userName.getText(), "PITER CHAILOVSKII");
+    }
+
+    public void check4ImagesDisplayed(WebDriver driver) {
+        assertEquals(benefitIcon.size(), 4);
+        for (WebElement icon : benefitIcon) {
+            assertTrue(icon.isDisplayed());
+        }
+    }
+
+    public void checkTextsUnderImages(IndexPageTextsEnum[] texts) {
+        assertEquals(benefitText.size(), texts.length);
+        for (int i = 0; i < texts.length; i++) {
+            assertEquals(benefitText.get(i).getText().replaceAll("\n", " "), texts[i].text);
+        }
+    }
+
+    public void checkHomePageHeaderDisplayed(WebDriver driver) {
+        assertTrue(header.isDisplayed());
+    }
+
+    public void checkHomePageMainTextDisplayed(WebDriver driver) {
+        assertTrue(mainText.isDisplayed());
+    }
+}
---
+++ src/test/java/homework3/PageObjectWebPageLoginTest.java
@@ -0,0 +1,59 @@
+package homework3;
+
+import enums.IndexPageTextsEnum;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pageObjects.hw3IndexPage;
+
+public class PageObjectWebPageLoginTest {
+
+    private WebDriver driver;
+    private hw3IndexPage indexPage;
+
+    //Prepare Webdriver
+    @BeforeClass
+    public void setUpPages() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        indexPage = PageFactory.initElements(driver, hw3IndexPage.class);
+    }
+
+    // Close Browser
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        driver.close();
+    }
+
+    //Create a new test in a new Java class, specify test name in accordance with checking functionality
+    @Test
+    public void testWebPageLogin() {
+        //1 Open test site by URL
+        driver.navigate().to("https://jdi-framework.github.io/tests");
+
+        //2 Assert Browser title
+        indexPage.checkPageTitle(driver);
+
+        //3 Perform login
+        indexPage.login("epam", "1234");
+
+        //4 Assert User name in the left-top side of screen that user is loggined
+        indexPage.checkUserIsLoggedIn(driver);
+
+        //5 Assert Browser title
+        indexPage.checkPageTitle(driver);
+
+        //6 Assert that there are 4 images on the Home Page and they are displayed
+        indexPage.check4ImagesDisplayed(driver);
+
+        //7 Assert that there are 4 texts on the Home Page and check them by getting texts
+        indexPage.checkTextsUnderImages(IndexPageTextsEnum.values());
+
+        //8 Assert that there are the main header and the text below it on the Home Page
+        indexPage.checkHomePageHeaderDisplayed(driver);
+        indexPage.checkHomePageMainTextDisplayed(driver);
+    }
+}
